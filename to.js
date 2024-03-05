b1907d2d358f0764a67a48364a2379483d4f4c3e
Ateeb Sheikh <Ateeb.Sheikh@CSPKAM00000843.mLogica.com>
2024-03-05 17:56:17 +0500

    updated

--- to.js
+++ to.js
@@ -1,15 +1,51 @@
 function toprint()
 {
+    // -----------printing data on div-----------
     var result = document.getElementById("info").value
     document.getElementById("datt").innerHTML = result
 
-    var userinput = document.getElementById("info").value
 
-    if(userinput.trim() === "")
-    {
-       window.alert("Must write the Information")
-    }
-    
-    
+    var store = document.getElementById("do4").value;
+    document.getElementById("datt").innerHTML = store;
+
+    //  for(i = 0; i < store.length; i++)
+    // {
+    //      document.getElementById("datt").innerHTML = store
+    // }
 }
+
+
+
+
+
+    // var store = document.getElementById("do4").value
+
+   
+
+    // var store = document.getElementById("do4").value
+    // document.getElementById("datt").innerHTML = store
+
+    // for(store = 0; store <= 100; store++)
+    // {
+    //     document.write(store)
+    // }
+    
+
+
+    //---------for pop up-------------
+    // var userinput = document.getElementById("info").value
+
+    // if(userinput.trim() === "")
+    // {
+    //    window.alert("Must write the Information")
+    // }
+
+
+    //  var store = document.getElementById("do4").value
+    
+    // if(store.filter() === " ")
+    // {
+    //    document.getElementById("datt").value = store
+    // }
+
 
