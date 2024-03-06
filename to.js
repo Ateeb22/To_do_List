bbe9dc16cfba79deaa63c8bca0b0de3e43443f59
Ateeb Sheikh <Ateeb.Sheikh@CSPKAM00000843.mLogica.com>
2024-03-06 17:59:20 +0500

    updated

--- to.js
+++ to.js
@@ -1,21 +1,69 @@
 function toprint()
 {
+    //---------for pop up-------------
+    var userinput = document.getElementById("info").value
+
+    if(userinput.trim() === "")
+    {
+       window.alert("Must write the Information")
+    }
+
     // -----------printing data on div-----------
-    var result = document.getElementById("info").value
-    document.getElementById("datt").innerHTML = result
+    var userInput = document.getElementById("info").value;
+    var currentContent = document.getElementById("datt").innerHTML;
+    document.getElementById("datt").innerHTML = currentContent + "<br>" + userInput;
+    document.getElementById("info").value = "";
 
 
-    var store = document.getElementById("do4").value;
-    document.getElementById("datt").innerHTML = store;
+
+
+
+
+
+
+
+
+
+
+
+    // var myNodelist = document.getElementsByTagName("LI");
+    // var i;
+    // for (i = 0; i < myNodelist.length; i++) {
+    //   var span = document.createElement("SPAN");
+    //   var txt = document.createTextNode("\u00D7");
+    //   span.className = "close";
+    //   span.appendChild(txt);
+    //   myNodelist[i].appendChild(span);
+    // }
+    
+
+    // var close = document.getElementsByClassName("close");
+    // var i;
+    // for (i = 0; i < close.length; i++) {
+    //   close[i].onclick = function() {
+    //     var div = this.parentElement;
+    //     div.style.display = "none";
+    //   }
+    // }
+   
+}
+
+
+
+
+    // -----------printing data on div-----------
+    // var result = document.getElementById("info").value
+    // document.getElementById("datt").innerHTML = result
+
+
+    // var store = document.getElementById("info").value;
+    // document.getElementById("datt").innerHTML = store;
+
 
     //  for(i = 0; i < store.length; i++)
     // {
     //      document.getElementById("datt").innerHTML = store
     // }
-}
-
-
-
 
 
     // var store = document.getElementById("do4").value
@@ -30,16 +78,7 @@ function toprint()
     //     document.write(store)
     // }
     
-
-
-    //---------for pop up-------------
-    // var userinput = document.getElementById("info").value
-
-    // if(userinput.trim() === "")
-    // {
-    //    window.alert("Must write the Information")
-    // }
-
+  
 
     //  var store = document.getElementById("do4").value
     
